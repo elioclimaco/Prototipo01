cdcf11f6def23d6ff4b94025fb5f21f28c56480a
eclimacoh <eclimacoh@gmail.com>
2019-03-26 21:46:56 -0500

    02. Cambio de color a la Barra de Navegación

--- src/components/layout/MainSidebar/MainSidebar.vue
+++ src/components/layout/MainSidebar/MainSidebar.vue
@@ -1,47 +1,52 @@
 <template>
-  <aside :class="['main-sidebar', 'col-12', 'col-md-3', 'col-lg-2', 'px-0', sidebarVisible ? 'open' : '']">
-      <div class="main-navbar">
-        <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0">
-          <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
-            <div class="d-table m-auto">
-              <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="@/assets/images/shards-dashboards-logo.svg" alt="Shards Dashboard">
-              <span v-if="!hideLogoText" class="d-none d-md-inline ml-1">Shards Dashboard</span>
-            </div>
-          </a>
-          <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none" @click="handleToggleSidebar()">
-            <i class="material-icons">&#xE5C4;</i>
-          </a>
-        </nav>
-      </div>
+    <aside :class="['main-sidebar', 'col-12', 'col-md-3', 'col-lg-2', 'px-0', sidebarVisible ? 'open' : '']">
 
-      <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
-        <div class="input-group input-group-seamless ml-3">
-          <div class="input-group-prepend">
-            <div class="input-group-text">
-              <i class="fas fa-search"></i>
-            </div>
-          </div>
-          <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
+        <!-- Barra de navegación -->
+        <div class="main-navbar">
+            <!-- eclimaco: original -->
+            <!-- <nav class="navbar align-items-stretch navbar-light bg-white flex-md-nowrap border-bottom p-0"> -->
+            <nav class="navbar align-items-stretch navbar-light bg-PJ_Fondo flex-md-nowrap border-bottom p-0">
+                <a class="navbar-brand w-100 mr-0" href="#" style="line-height: 25px;">
+                    <div class="d-table m-auto">
+                    <img id="main-logo" class="d-inline-block align-top mr-1" style="max-width: 25px;" src="@/assets/images/shards-dashboards-logo.svg" alt="Shards Dashboard">
+                    <span v-if="!hideLogoText" class="d-none d-md-inline ml-1">Shards Dashboard</span>
+                    </div>
+                </a>
+
+                <a class="toggle-sidebar d-sm-inline d-md-none d-lg-none" @click="handleToggleSidebar()">
+                    <i class="material-icons">&#xE5C4;</i>
+                </a>
+            </nav>
         </div>
-      </form>
 
-      <div class="nav-wrapper">
-          <d-nav class="flex-column">
-            <li v-for="(item, navItemIdx) in items" :key="navItemIdx" class="nav-item dropdown">
-              <d-link :class="['nav-link', item.items && item.items.length ? 'dropdown-toggle' : '']" :to="item.to" v-d-toggle="`snc-${navItemIdx}`">
-                <div class="item-icon-wrapper" v-if="item.htmlBefore" v-html="item.htmlBefore" />
-                <span v-if="item.title">{{ item.title }}</span>
-                <div class="item-icon-wrapper" v-if="item.htmlAfter" v-html="item.htmlAfter" />
-              </d-link>
-              <d-collapse v-if="item.items && item.items.length" :id="`snc-${navItemIdx}`" class="dropdown-menu dropdown-menu-small" accordion="sidebar-items-accordion">
-                <d-dropdown-item v-for="(subItem, subItemIdx) in item.items" :key="subItemIdx" :href="subItem.href" :to="subItem.to">
-                  {{ subItem.title }}
-                </d-dropdown-item>
-              </d-collapse>
-            </li>
-          </d-nav>
-      </div>
-  </aside>
+        <form action="#" class="main-sidebar__search w-100 border-right d-sm-flex d-md-none d-lg-none">
+            <div class="input-group input-group-seamless ml-3">
+            <div class="input-group-prepend">
+                <div class="input-group-text">
+                <i class="fas fa-search"></i>
+                </div>
+            </div>
+            <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
+            </div>
+        </form>
+
+        <div class="nav-wrapper">
+            <d-nav class="flex-column">
+                <li v-for="(item, navItemIdx) in items" :key="navItemIdx" class="nav-item dropdown">
+                <d-link :class="['nav-link', item.items && item.items.length ? 'dropdown-toggle' : '']" :to="item.to" v-d-toggle="`snc-${navItemIdx}`">
+                    <div class="item-icon-wrapper" v-if="item.htmlBefore" v-html="item.htmlBefore" />
+                    <span v-if="item.title">{{ item.title }}</span>
+                    <div class="item-icon-wrapper" v-if="item.htmlAfter" v-html="item.htmlAfter" />
+                </d-link>
+                <d-collapse v-if="item.items && item.items.length" :id="`snc-${navItemIdx}`" class="dropdown-menu dropdown-menu-small" accordion="sidebar-items-accordion">
+                    <d-dropdown-item v-for="(subItem, subItemIdx) in item.items" :key="subItemIdx" :href="subItem.href" :to="subItem.to">
+                    {{ subItem.title }}
+                    </d-dropdown-item>
+                </d-collapse>
+                </li>
+            </d-nav>
+        </div>
+    </aside>
 </template>
 
 <script>
@@ -83,12 +88,29 @@ export default {
 </script>
 
 <style lang="scss">
-  .main-sidebar {
-    .item-icon-wrapper {
-      display: inline-block;
+    .main-sidebar {
+        .item-icon-wrapper {
+            display: inline-block;
+        }
+        .dropdown-menu {
+            display: block;
+        }
     }
-    .dropdown-menu {
-      display: block;
-    }
-  }
+
+// Color de los ítems del SideBar
+// $fiord-blue:   #3D5170 !default;
+
+// eclimacoh: Personalización de la SideBar.
+.main-sidebar {
+  top: 0;
+}
+
+// eclimacoh: Revisar.
+/*
+https://www.canva.com/learn/website-color-schemes/
+https://htmlcolorcodes.com/es/selector-de-color/#
+http://rogerdudler.github.io/git-guide/index.es.html
+https://gist.github.com/BCasal/026e4c7f5c71418485c1
+https://github.com/javierbrk/reconocedordebilletes/wiki/Tutorial-Fork-y-Sincronizacion
+*/
 </style>
